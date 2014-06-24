5105cd5a8ff5501e17115e6b762f0ce5b6f5844a
amoblin@mpb <amoblin@gmail.com>
2014-06-24 11:18:37 +0800

    markdown package

--- media/packages/markdown/converts/md2html.sh
+++ media/packages/markdown/converts/md2html.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # author: amoblin <amoblin@gmail.com>
-# file name: convert.init.sh
+# file name: md2html.sh
 # create date: 2013-03-07 15:15:19
 # This file is created from ~/.marboo/source/media/bin/default.init.sh
 # 本文件由 ~/.marboo/source/media/bin/default.init.sh　复制而来
--- media/packages/markdown/starts/default.md
+++ media/packages/markdown/starts/default.md
@@ -1,3 +1,4 @@
 # {{title}}
+
 <!-- create time: {{date}}  -->
 
